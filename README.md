64e58317143ff0e68a9c961af579339ea1f95fd5
Sara OC <saramoo@hotmail.com>
2015-04-30 16:25:44 +0100

    repo set up + search result and term initalise empty

---
+++ README.md
@@ -0,0 +1,7 @@
+Angular + Jasmine + Bower + Grunt
+
+with @saramoohead and @m1lena
+
+https://github.com/makersacademy/course/blob/master/further_javascript/github_profiles.md
+
+30 April 2015
---
+++ js/app.js
@@ -0,0 +1,5 @@
+var githubUserSearch = angular.module('GitUserSearch', ['ngResource']);
+// an angular module is a collection services, directives, controllers and
+// angular.module is used to configure the $injector
+// ngResource is for interacting with RESTful services via that $resource service
+
