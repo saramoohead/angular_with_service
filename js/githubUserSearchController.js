2d24991f5ff2ee8e3dbc14bc1bb2a2a15058a74d
Sara OC <saramoo@hotmail.com>
2015-04-30 17:23:06 +0100

    bind search term to controller

--- js/githubUserSearchController.js
+++ js/githubUserSearchController.js
@@ -2,6 +2,7 @@ githubUserSearch.controller('GitUserSearchController', [function() {
     var self = this;
 
     self.doSearch = function() {
+        console.log(self.searchTerm);
         self.searchResult = {
             "items": [
                 {
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -27,7 +27,7 @@ describe('GitUserSearchController', function() {
       ];
 
       it('displays search results', function() {
-        ctrl.searchTerm = 'hello'
+        ctrl.searchTerm = 'hello';
         ctrl.doSearch();
         expect(ctrl.searchResult.items).toEqual(items);
       });
