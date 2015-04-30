38574c1bd569d68246c6c0332359ffa198befc5f
Sara OC <saramoo@hotmail.com>
2015-04-30 19:14:49 +0100

    wire controller to factory

--- js/githubUserSearchController.js
+++ js/githubUserSearchController.js
@@ -1,13 +1,12 @@
-githubUserSearch.controller('GitUserSearchController', ['$resource',function($resource) {
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
 
-    var searchResource = $resource('https://api.github.com/search/users?access_token=a17eee887ac0a92ed1407d7943647dd94fa5a587');
+    // var searchResource = $resource('https://api.github.com/search/users?access_token=a17eee887ac0a92ed1407d7943647dd94fa5a587');
     var self = this;
 
     self.doSearch = function() {
-        if(self.searchTerm !=='') {
-            self.searchResult = searchResource.get(
-                { q: self.searchTerm }
-            );
-        }
+        Search.query(self.searchTerm)
+            .then(function(response) {
+                self.searchResult = response.data;
+        });
     };
 }]);
